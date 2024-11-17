be697f06b6a1dbb2e87d9963715e9a4d897bf78c
227Faddi <falukhouma27@gmail.com>
2024-11-17 17:28:58 -0500

    feat: setup react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+
+import RootLayout from "./layout/RootLayout";
 import DashboardPage from "./pages/DashboardPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  return (
-    <>
-      <DashboardPage />
-    </>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="dashboard" element={<DashboardPage />} />
+      </Route>
+    )
   );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
