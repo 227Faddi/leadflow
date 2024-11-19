7fcc17314c691b4cc70639ee5d930a890fac7f4f
227Faddi <falukhouma27@gmail.com>
2024-11-19 11:52:46 -0500

    refactor: move info section from homepage to its own component

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,7 @@
 import { Link } from "react-router-dom";
-import { FaUserPlus } from "react-icons/fa6";
-import { FaUsers } from "react-icons/fa6";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import InfoSection from "../components/InfoSection";
 
 const HomePage = () => {
   return (
@@ -32,35 +31,7 @@ const HomePage = () => {
             </Link>
           </div>
         </section>
-        <section className="flex flex-col items-center justify-center space-y-8 px-4 md:px-10 lg:px-56 mb-52">
-          <div className="bg-gray-100 p-10 md:p-20 rounded-2xl flex flex-col md:flex-row justify-between gap-8">
-            <div className="flex flex-col gap-12">
-              <div className="space-y-4">
-                <FaUserPlus className="text-blue-700" size={35} />
-                <h3 className="font-bold">Lorem, ipsum dolor.</h3>
-                <p className="md:w-2/3 text-gray-500">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Aspernatur officiis mollitia, repellat facere laborum culpa.
-                </p>
-              </div>
-              <div className="space-y-4">
-                <FaUsers className="text-blue-700" size={35} />
-                <h3 className="font-bold">Lorem, ipsum dolor.</h3>
-                <p className="md:w-2/3 text-gray-500">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Aspernatur officiis mollitia, repellat facere laborum culpa.
-                </p>
-              </div>
-            </div>
-            <div>
-              <img
-                className="w-full h-full"
-                src="https://placehold.co/600x400"
-                alt=""
-              />
-            </div>
-          </div>
-        </section>
+        <InfoSection />
       </main>
       <Footer />
     </div>
