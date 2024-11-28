52a2e73ca85e41c1b1d50ed383b3416148aeb9b0
227Faddi <falukhouma27@gmail.com>
2024-11-27 21:48:45 -0500

    feat: add lead post route

--- server/controllers/leadsControllers.js
+++ server/controllers/leadsControllers.js
@@ -5,4 +5,9 @@ export default {
     const leads = await Lead.findAll();
     res.status(200).send(leads);
   },
+  addLead: async (req, res) => {
+    const newLead = req.body;
+    await Lead.create(newLead);
+    res.status(200).send({ message: 'Lead added successfully' });
+  },
 };
--- server/routes/leadsRoutes.js
+++ server/routes/leadsRoutes.js
@@ -4,5 +4,6 @@ import leadsControllers from '../controllers/leadsControllers.js';
 const router = express.Router();
 
 router.get('/', leadsControllers.getLeads);
+router.post('/add', leadsControllers.addLead);
 
 export default router;
