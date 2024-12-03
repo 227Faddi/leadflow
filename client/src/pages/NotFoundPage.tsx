b80862f265784117eda0ef5b17751af6d441c476
227Faddi <falukhouma27@gmail.com>
2024-12-03 08:55:16 -0500

    fix: adjust responsive design for not found page

--- client/src/pages/NotFoundPage.tsx
+++ client/src/pages/NotFoundPage.tsx
@@ -5,9 +5,9 @@ const NotFoundPage = () => {
   return (
     <div className="h-screen flex flex-col">
       <Header />
-      <main className="flex-1 flex flex-col items-center justify-center space-y-6">
-        <p className="text-7xl font-semibold text-blue-700">404</p>
-        <h1 className="text-5xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+      <main className="flex-1 flex flex-col text-center items-center justify-center space-y-6 px-12">
+        <p className="text-5xl md:text-7xl font-semibold text-blue-700">404</p>
+        <h1 className="font-bold tracking-tight text-gray-900 text-xl sm:text-2xl md:text-5xl">
           Page not found
         </h1>
         <p className="text-lg leading-7 text-gray-600">
@@ -15,7 +15,7 @@ const NotFoundPage = () => {
         </p>
         <a
           href="/"
-          className="text-white bg-gradient-to-r from-green-500 via-green-600 to-green-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 shadow-lg shadow-green-500/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+          className="text-white bg-gradient-to-r from-green-500 via-green-600 to-green-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 shadow-lg shadow-green-500/50 font-medium rounded-lg text-sm px-5 py-2.5"
         >
           Go Back Home
         </a>
