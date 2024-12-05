156a0fc872dcf8c0eccc35066f6c77599fad94e6
227Faddi <falukhouma27@gmail.com>
2024-12-05 11:13:06 -0500

    chore: change footer link to github repo

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Lead FLow [ IN DEVELOPMENT]
+# Lead Flow [ IN DEVELOPMENT ]
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -28,7 +28,7 @@ const Footer = () => {
             </span>
           </a>
           <a
-            href="https://github.com/227Faddi/leads-track-react"
+            href="https://github.com/227Faddi/leadflow"
             className="ml-2 relative p-0.5 inline-flex items-center justify-center overflow-hidden text-gray-900 rounded-full group bg-gradient-to-br from-green-600 to-blue-500 group-hover:from-green-600 group-hover:to-blue-500 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 w-14 h-14"
           >
             <span className="text-lg relative transition-all ease-in duration-75 bg-white rounded-full group-hover:bg-opacity-0 flex items-center justify-center w-full h-full">
