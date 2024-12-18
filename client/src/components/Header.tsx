ad9ee7aeebfe364ebdf55b3f093fc4d17f0450c1
227Faddi <falukhouma27@gmail.com>
2024-12-18 12:28:21 -0500

    feat: add login guest mutation

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,6 +1,31 @@
+import { useMutation } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
 import { IoIosContacts } from "react-icons/io";
+import { loginPost } from "../services/api/auth";
+import toast from "react-hot-toast";
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const guest = {
+    email: "guest@gmail.com",
+    password: "helloworld",
+  };
+
+  const { mutateAsync: loginMutation } = useMutation({
+    mutationFn: loginPost,
+  });
+
+  const handleGuestLogin = async () => {
+    await loginMutation(guest, {
+      onSuccess: () => {
+        toast.success("Logged in as a Guest");
+        navigate("/dashboard");
+      },
+      onError: () => toast.error("An error occurred. Please try again."),
+    });
+  };
+
   return (
     <header className="flex flex-col sm:flex-row gap-8 justify-around items-center py-10">
       <a
@@ -10,12 +35,12 @@ const Header = () => {
         <IoIosContacts className="text-green-700" size={35} />
         LeadFlow
       </a>
-      <a
-        href="/dashboard"
+      <button
+        onClick={handleGuestLogin}
         className="text-white bg-gradient-to-r from-blue-500 to-green-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 shadow-lg shadow-blue-500/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
       >
         Try as Guest
-      </a>
+      </button>
     </header>
   );
 };
