3c4ef34e81053d98bac13ce430a46b1624a45b07
227Faddi <falukhouma27@gmail.com>
2025-01-07 18:36:36 -0500

    style: change homepage image and texts

--- client/src/components/HomePageInfo.tsx
+++ client/src/components/HomePageInfo.tsx
@@ -5,29 +5,31 @@ const HomePageInfo = () => {
   return (
     <section className="flex flex-col items-center justify-center space-y-8 px-4 md:px-10 lg:px-56 mb-52">
       <div className="bg-gray-100 p-10 md:p-20 rounded-2xl flex flex-col md:flex-row justify-between space-y-12">
-        <div className="flex flex-col items-center justify-center gap-12">
+        <div className="flex flex-col items-center justify-center gap-12 flex-1">
           <div className="space-y-2">
             <FaUserPlus className="text-blue-700" size={35} />
             <h3 className="font-bold">Effortless Lead Tracking</h3>
             <p className="md:w-2/3 text-gray-500">
-              Keep all your leads in one place and update statuses in real-time
-              to stay on top of your sales pipeline.
+              Keep your leads organized in one place and update statuses
+              instantly. Stay on top of your sales pipeline and never miss an
+              opportunity to close a deal.
             </p>
           </div>
           <div className="space-y-2">
             <FaChartColumn className="text-blue-700" size={35} />
             <h3 className="font-bold">Comprehensive Analytics</h3>
             <p className="md:w-2/3 text-gray-500">
-              Get insights to optimize your sales and marketing efforts, helping
-              you make smarter decisions.
+              Gain valuable insights into your sales performance. Identify
+              trends, refine strategies, and make smarter, data-driven decisions
+              to boost results.
             </p>
           </div>
         </div>
-        <div className="flex items-center">
+        <div className="flex items-center flex-1">
           <img
             className="w-full object-cover"
-            src="/assets/img/laptop.png"
-            alt=""
+            src="/assets/img/desktop.webp"
+            alt="dashboard preview"
           />
         </div>
       </div>
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -9,12 +9,13 @@ const HomePage = () => {
       <Header />
       <main className="flex-1 flex flex-col items-center justify-center my-20 md:my-48 space-y-32 md:space-y-40">
         <section className="flex flex-col items-center justify-center space-y-14">
-          <div className="text-center max-w-[75%] space-y-4">
+          <div className="text-center max-w-[75%] space-y-4 flex flex-col items-center">
             <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold">
               Simplify Your Lead Management Process
             </h1>
-            <p className="text-xl sm:text-2xl md:text-3xl text-gray-500 font-thin">
-              Track, manage, and analyze your leads in one powerful platform
+            <p className="text-xl sm:text-2xl md:text-3xl text-gray-500 font-thin max-w-3xl">
+              Easily organize, track, and analyze your potential clients to stay
+              ahead and close more deals, all in one powerful platform.
             </p>
           </div>
           <div className="mt-0 space-x-8">
