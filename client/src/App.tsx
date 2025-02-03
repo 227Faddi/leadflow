2052ee1fa31e8546e73f2d69efa926dae30bf8d4
227Faddi <falukhouma27@gmail.com>
2025-02-02 19:04:22 -0500

    test: enable protected routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -18,25 +18,27 @@ import LoginPage from "./pages/LoginPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import SettingsPage from "./pages/SettingsPage";
 import SignupPage from "./pages/SignupPage";
+import EnsureAuth from "./utils/auth/EnsureAuth";
+import EnsureGuest from "./utils/auth/EnsureGuest";
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<RootLayout />}>
-        {/* <Route element={<EnsureAuth />}> */}
-        <Route element={<DashboardLayout />}>
-          <Route path="dashboard" element={<DashboardPage />} />
-          <Route path="analytics" element={<AnalyticsPage />} />
-          <Route path="settings" element={<SettingsPage />} />
-          <Route path="add" element={<AddLeadPage />} />
-          <Route path="edit/:id" element={<EditLeadPage />} />
+        <Route element={<EnsureAuth />}>
+          <Route element={<DashboardLayout />}>
+            <Route path="dashboard" element={<DashboardPage />} />
+            <Route path="analytics" element={<AnalyticsPage />} />
+            <Route path="settings" element={<SettingsPage />} />
+            <Route path="add" element={<AddLeadPage />} />
+            <Route path="edit/:id" element={<EditLeadPage />} />
+          </Route>
         </Route>
-        {/* </Route> */}
-        {/* <Route element={<EnsureGuest />}> */}
-        <Route path="/" element={<HomePage />} />
-        <Route path="signup" element={<SignupPage />} />
-        <Route path="login" element={<LoginPage />} />
-        {/* </Route> */}
+        <Route element={<EnsureGuest />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="signup" element={<SignupPage />} />
+          <Route path="login" element={<LoginPage />} />
+        </Route>
 
         <Route path="*" element={<NotFoundPage />} />
       </Route>
