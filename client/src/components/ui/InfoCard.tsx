2b3ed242ce1a5eb10b9da569279ba86d037b018d
227Faddi <falukhouma27@gmail.com>
2025-02-17 11:56:49 -0500

    fix: change text size to ensure responsiveness

--- client/src/components/ui/InfoCard.tsx
+++ client/src/components/ui/InfoCard.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 const InfoCard = ({ text, data, icon }: Props) => {
   return (
-    <div className="flex-1 w-full border-slate-200 border p-3 lg:p-6 bg-gray-50 rounded-lg shadow-lg dark:bg-gray-900 dark:border-slate-700">
+    <div className="flex-1 w-full border-slate-200 border p-3 xl:p-6 bg-gray-50 rounded-lg shadow-lg dark:bg-gray-900 dark:border-slate-700">
       <motion.div
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
@@ -20,7 +20,7 @@ const InfoCard = ({ text, data, icon }: Props) => {
         <div className="p-3 bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 shadow-lg shadow-blue-500/50 text-white rounded-full dark:from-gray-100 dark:via-gray-200 dark:to-gray-300">
           {icon}
         </div>
-        <h4 className="text-lg lg:text-2xl mx-5 font-semibold text-gray-900 dark:text-white">
+        <h4 className="text-lg xl:text-2xl mx-5 font-semibold text-gray-900 dark:text-white">
           {text} : {data}
         </h4>
       </motion.div>
